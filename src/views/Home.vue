8322fe4d1cf2dd17f9b7ae75added97dee0f2c90
mohammad <m.sabah.farhadi@gmail.com>
2021-09-12 18:14:41 +0430

    add some style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,49 @@
 <template>
-  <div class="home">
-    <div v-if="todos.length > 0">{{ todos.filter(todo => todo.status === "waiting").length }} items left</div>
-    <form @submit.prevent="addTodo">
-      <input type="text" v-model="title">
-      <textarea name="" id="" cols="30" rows="10" v-model="description"></textarea>
-      <button>add</button>
-    </form>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        <div v-if="todo.status === 'waiting'">
-          <button @click="handleChangeStatus(todo.id, true)">complete</button>
+  <div class="container">
+    <!--//! START: todo header -->
+    <div class="todo-header">
+      <h1>Todo</h1>
+      <div
+        class="cursor-pointer"
+        @click="toggleTheme"
+        aria-label="Toggle themes"
+      >
+        <div v-if="theme !== 'darkMode'">
+          <img src="../assets/images/icon-moon.svg" alt="dark mode" />
         </div>
         <div v-else>
-          <button @click="handleChangeStatus(todo.id, false)">todo</button>
+          <img src="../assets/images/icon-sun.svg" alt="light mode" />
         </div>
-        <button @click="removeTodo(todo.id)">remove</button>
-        <h4 :class="[todo.status === 'waiting' ? 'waiting' : 'completed']">{{ todo.title }}</h4>
-        <p>{{ todo.description }}</p>
-        <p>{{ todo.status }}</p>
+      </div>
+    </div>
+    <!--//? END: todo header -->
+    <!--//! START: new todo form -->
+    <form @submit.prevent="addTodo">
+      <div class="new-todo">
+        <button>
+          <span class="circle"></span>
+        </button>
+        <input type="text" v-model="title" />
+      </div>
+    </form>
+    <!--//? END: new todo form -->
+
+    <ul class="todos-list">
+      <li v-for="todo in todos" :key="todo.id">
+        <div>
+          <button v-if="todo.status === 'completed'" class="completed" @click="handleChangeStatus(todo.id, false)">
+            <img src="../assets/images/icon-check.svg" alt="Completed">
+          </button>
+          <button v-else @click="handleChangeStatus(todo.id, true)">
+            <span class="circle"></span>
+          </button>
+        </div>
+        <p :class="[todo.status === 'waiting' ? '' : 'completed']">
+          {{ todo.title }}
+        </p>
+        <button @click="removeTodo(todo.id)" class="remove-todo">
+          <img src="../assets/images/icon-check.svg" alt="Delete">
+        </button>
       </li>
     </ul>
     <div v-if="todos.length">
@@ -26,46 +52,51 @@
       <button @click="filterTodos('')">all</button>
     </div>
     <button @click="clearCompletedStatus()">remove all completed todo</button>
+    <div v-if="todos.length > 0">
+      {{ todos.filter((todo) => todo.status === "waiting").length }} items left
+    </div>
   </div>
 </template>
 
 <script>
-
-import {ref, reactive, computed} from "@vue/reactivity";
-import {watch, watchEffect} from '@vue/runtime-core';
-import axios from 'axios';
+import { ref, reactive, computed } from "@vue/reactivity";
+import { watch, watchEffect } from "@vue/runtime-core";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {},
   setup() {
     const todos = ref([]);
     const title = ref(null);
-    const description = ref(null);
-    const status = ref("waiting")
+    // const description = ref(null);
+    const status = ref("waiting");
+    const theme = ref("");
 
     watchEffect(async () => {
+      let localTheme = localStorage.getItem("theme"); //gets stored theme value if any
+      document.documentElement.setAttribute("data-theme", localTheme); // updates the data-theme attribute
       try {
-        const {data} = await axios.get("http://localhost:3000/todos");
-        todos.value = data
+        const { data } = await axios.get("http://localhost:3000/todos");
+        todos.value = data;
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-    })
+    });
 
     const addTodo = async () => {
       try {
-        const {data} = await axios.post("http://localhost:3000/todos", {
+        const { data } = await axios.post("http://localhost:3000/todos", {
           id: Math.random() * 99999999,
           title: title.value,
-          description: description.value,
-          status: status.value
+          // description: description.value,
+          status: status.value,
         });
         todos.value = [...todos.value, data];
         title.value = "";
-        description.value = ""
+        // description.value = "";
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
       // const response = await fetch("http://localhost:3000/todos", {
       //   method: 'POST',
@@ -85,14 +116,16 @@ export default {
       // })
       // todos.value = [...todos.value, response]
       // console.log(response)
-    }
+    };
 
     const removeTodo = async (id) => {
       try {
-        const {status} = await axios.delete(`http://localhost:3000/todos/${id}`);
-        todos.value = todos.value.filter(todo => todo.id !== id)
+        const { status } = await axios.delete(
+          `http://localhost:3000/todos/${id}`
+        );
+        todos.value = todos.value.filter((todo) => todo.id !== id);
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
       // const response = await fetch(`http://localhost:3000/todos/${id}`, {
       //   method: 'DELETE',
@@ -105,40 +138,45 @@ export default {
       //   }
       // })
       // return [...todos.value, response.json()]
-    }
+    };
 
     const handleChangeStatus = async (id, isCompleted) => {
       try {
-        const {data} = await axios.patch(`http://localhost:3000/todos/${id}`, {status: isCompleted ? "completed" : "waiting"});
-        let todo = todos.value.find(todo => todo.id === id);
-        todo.status = isCompleted ? "completed" : "waiting"
+        const { data } = await axios.patch(
+          `http://localhost:3000/todos/${id}`,
+          { status: isCompleted ? "completed" : "waiting" }
+        );
+        let todo = todos.value.find((todo) => todo.id === id);
+        todo.status = isCompleted ? "completed" : "waiting";
         // todos.value = [...todos.value, data]
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-    }
+    };
 
     const filterTodos = async (status) => {
       try {
-        const {data} = await axios.get(`http://localhost:3000/todos${status ? `?status=${status}` : ""}`);
-        todos.value = data
+        const { data } = await axios.get(
+          `http://localhost:3000/todos${status ? `?status=${status}` : ""}`
+        );
+        todos.value = data;
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-    }
+    };
 
     const clearCompletedStatus = async () => {
       try {
-        let reqs = []
-        todos.value.filter(todo => {
+        let reqs = [];
+        todos.value.filter((todo) => {
           if (todo.status === "completed") {
-            reqs.push(axios.delete(`http://localhost:3000/todos/${todo.id}`))
+            reqs.push(axios.delete(`http://localhost:3000/todos/${todo.id}`));
           }
-        })
-        const {status} = await axios.all(reqs);
-        if (status === 200) {
-          todos.value = todos.value.filter(todo => todo.status !== "completed")
-        }
+        });
+        const { status } = await axios.all(reqs);
+        // if (status === 200) {
+        todos.value = todos.value.filter((todo) => todo.status !== "completed");
+        // }
         // todos.value = data
         // todos.value.filter(async todo => {
         //   if (todo.status === "completed") {
@@ -147,32 +185,33 @@ export default {
         //   }
         // })
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-    }
+    };
 
+    const toggleTheme = () => {
+      theme.value = theme.value == "darkMode" ? "" : "darkMode"; //toggles theme value
+      document.documentElement.setAttribute("data-theme", theme.value); // sets the data-theme attribute
+      localStorage.setItem("theme", theme.value); // stores theme value on local storage
+    };
 
     return {
       todos,
       title,
-      description,
+      // description,
       status,
       addTodo,
       removeTodo,
       handleChangeStatus,
       filterTodos,
-      clearCompletedStatus
-    }
+      clearCompletedStatus,
+      theme,
+      toggleTheme,
+    };
   },
-}
+};
 </script>
 
 <style lang="scss">
-.waiting {
-  color: red;
-}
 
-.completed {
-  text-decoration: line-through;
-}
 </style>
